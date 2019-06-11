ead2415a68480d4d450aa5c8a3d4479fd9729183
chandanpreetpunia1990 <51385227+chandanpreetpunia1990@users.noreply.github.com>
2019-06-10 22:15:36 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,17 @@
+print ('My name is "Chandanpreet"')
+
+print ('This is my first program')
+x1 = int(input("Value 1"))
+
+print ("value 1")
+
+x2 = int(input("Value 2"))
+value2 = x2
+print ("value 2")
+
+
+
+
+
+
+ 
